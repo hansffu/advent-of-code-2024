cf5b44fc5af17810395db20bc0d79bae4a8a8d07
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-01 19:21:56 +0100

    day 1 part 2

--- src/Solutions/Day1.hs
+++ src/Solutions/Day1.hs
@@ -1,15 +1,15 @@
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Solutions.Day1 (day1) where
 
+import Data.IntMap (findWithDefault, fromListWith)
 import Data.List (sort, transpose)
 import Lib.Solution
 import Lib.Utils (readInt)
 
 day1 :: Solution Int Int
 day1 = Solution 1 part1 part2
-
-test = testSolution day1
 
 part1 :: [String] -> IO Int
 part1 rows =
@@ -20,4 +20,8 @@ part1 rows =
   sortedBs = sort $ readInt <$> bs
 
 part2 :: [String] -> IO Int
-part2 = todo
+part2 rows = do
+  return $ sum $ (\a -> a * findWithDefault 0 a counts) <$> as
+ where
+  [as, bs] = transpose $ (readInt <$>) . words <$> rows
+  counts = fromListWith (+) $ (,1) <$> bs
