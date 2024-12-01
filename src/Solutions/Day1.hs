e2a09f375ab6d0463353083d19f968580900f761
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-01 14:08:27 +0100

    day 1 part 1

--- src/Solutions/Day1.hs
+++ src/Solutions/Day1.hs
@@ -1,13 +1,23 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
 module Solutions.Day1 (day1) where
 
+import Data.List (sort, transpose)
 import Lib.Solution
+import Lib.Utils (readInt)
 
 day1 :: Solution Int Int
 day1 = Solution 1 part1 part2
 
+test = testSolution day1
+
 part1 :: [String] -> IO Int
-part1 = todo
+part1 rows =
+  return $ sum $ zipWith (\a b -> abs (a - b)) sortedAs sortedBs
+ where
+  [as, bs] = transpose $ words <$> rows
+  sortedAs = sort $ readInt <$> as
+  sortedBs = sort $ readInt <$> bs
 
 part2 :: [String] -> IO Int
 part2 = todo
-
