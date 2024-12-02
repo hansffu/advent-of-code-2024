e01ac520f87072fc12f7bc0fa2d49ef7d073fa32
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-02 18:00:51 +0100

    haskell nørding

--- src/Solutions/Day2.hs
+++ src/Solutions/Day2.hs
@@ -2,6 +2,7 @@ module Solutions.Day2 (day2) where
 
 import Lib.Parser (Parser)
 import Lib.Solution
+import Lib.Utils (dropIndex)
 import Text.Megaparsec
 import Text.Megaparsec.Char (hspace1, newline)
 import Text.Megaparsec.Char.Lexer (decimal)
@@ -10,29 +11,34 @@ day2 :: Solution [[Int]] Int Int
 day2 = Solution 2 parser part1 part2
 
 part1 :: [[Int]] -> IO Int
-part1 input = return $ length $ filter isSafe input
+part1 input = return $ length $ filter isSafe' input
 
 part2 :: [[Int]] -> IO Int
 part2 input = return $ length $ filter (anySafe . combinations) input
  where
   anySafe :: [[Int]] -> Bool
-  anySafe = any isSafe
+  anySafe = any isSafe'
 
 parser :: Parser [[Int]]
 parser = many $ decimal `sepBy` hspace1 <* newline
 
 combinations :: [Int] -> [[Int]]
-combinations xs = [let (a, b) = splitAt i xs in a ++ tail b | i <- [0 .. (length xs - 1)]]
+combinations xs = flip dropIndex xs <$> [0 .. (length xs - 1)]
 
-isSafe :: [Int] -> Bool
-isSafe xs = safeIncrease xs || safeDecrease xs
+isSafe' :: [Int] -> Bool
+isSafe' xs = isSafe (Increase <$> xs) || isSafe (Decrease <$> xs)
 
-safeDecrease :: [Int] -> Bool
-safeDecrease [_] = True
-safeDecrease (x : y : xs) = x > y && x - y <= 3 && safeDecrease (y : xs)
-safeDecrease _ = error "too small list"
+newtype Increase a = Increase a
+newtype Decrease a = Decrease a
 
-safeIncrease :: [Int] -> Bool
-safeIncrease [_] = True
-safeIncrease (x : y : xs) = x < y && y - x <= 3 && safeIncrease (y : xs)
-safeIncrease _ = error "too small list"
+class SafePattern a where
+  isSafeStep :: a -> a -> Bool
+  isSafe :: [a] -> Bool
+  isSafe (x : y : xs) = isSafeStep x y && isSafe (y : xs)
+  isSafe _ = True
+
+instance (Num a, Ord a) => SafePattern (Increase a) where
+  isSafeStep (Increase x) (Increase y) = x < y && y - x <= 3
+
+instance (Num a, Ord a) => SafePattern (Decrease a) where
+  isSafeStep (Decrease x) (Decrease y) = x > y && x - y <= 3
