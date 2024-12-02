ba0e2d49560b837eb7e57df222fa981eff675a07
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-02 14:53:12 +0100

    day 2 part 2

--- src/Solutions/Day2.hs
+++ src/Solutions/Day2.hs
@@ -10,20 +10,29 @@ day2 :: Solution [[Int]] Int Int
 day2 = Solution 2 parser part1 part2
 
 part1 :: [[Int]] -> IO Int
-part1 input = return $ length $ filter (\xs -> safeIncrease xs || safeDecrease xs) input
- where
-  safeDecrease :: [Int] -> Bool
-  safeDecrease [_] = True
-  safeDecrease (x : y : xs) = x > y && x - y <= 3 && safeDecrease (y : xs)
-  safeDecrease _ = error "too small list"
-
-  safeIncrease :: [Int] -> Bool
-  safeIncrease [_] = True
-  safeIncrease (x : y : xs) = x < y && y - x <= 3 && safeIncrease (y : xs)
-  safeIncrease _ = error "too small list"
+part1 input = return $ length $ filter isSafe input
 
 part2 :: [[Int]] -> IO Int
-part2 = todo
+part2 input = return $ length $ filter (anySafe . combinations) input
+ where
+  anySafe :: [[Int]] -> Bool
+  anySafe = any isSafe
 
 parser :: Parser [[Int]]
 parser = many $ decimal `sepBy` hspace1 <* newline
+
+combinations :: [Int] -> [[Int]]
+combinations xs = [let (a, b) = splitAt i xs in a ++ tail b | i <- [0 .. (length xs - 1)]]
+
+isSafe :: [Int] -> Bool
+isSafe xs = safeIncrease xs || safeDecrease xs
+
+safeDecrease :: [Int] -> Bool
+safeDecrease [_] = True
+safeDecrease (x : y : xs) = x > y && x - y <= 3 && safeDecrease (y : xs)
+safeDecrease _ = error "too small list"
+
+safeIncrease :: [Int] -> Bool
+safeIncrease [_] = True
+safeIncrease (x : y : xs) = x < y && y - x <= 3 && safeIncrease (y : xs)
+safeIncrease _ = error "too small list"
