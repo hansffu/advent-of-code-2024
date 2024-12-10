4cd3afcd7ee12b7508d809ddb41461a9c717364a
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-10 23:11:07 +0100

    day 10 part 2

--- src/Solutions/Day10.hs
+++ src/Solutions/Day10.hs
@@ -5,6 +5,7 @@ import Text.Megaparsec.Char (digitChar, eol)
 import Data.Array.Base ((!?))
 import Data.Char (digitToInt)
 import Data.List (nub)
+import Data.List.Extra (groupOn)
 import Data.Maybe (catMaybes)
 import Lib.Dfs (dfs)
 import Lib.Parser (Parser)
@@ -17,30 +18,42 @@ solution = Solution 10 parser part1 part2
 type Node = (Int, (Int, Int))
 part1 :: Input -> IO Int
 part1 input = do
-  return $ sum $ length . nub . filter (\(n, _) -> n == 9) . dfs getNeighbours <$> trailHeads
+  return $ sum $ length . nub . filter (\(n, _) -> n == 9) . dfs (getNeighbours arr) <$> trailHeads
  where
   arr = toArray2d input
   trailHeads = findIndexes2d (== 0) input
-  getNeighbours :: Node -> [Node]
-  getNeighbours (item, (y, x)) =
-    filter ((== item) . pred . fst) $
-      catMaybes
-        [ safe2dLookup arr (y + 1, x)
-        , safe2dLookup arr (y - 1, x)
-        , safe2dLookup arr (y, x + 1)
-        , safe2dLookup arr (y, x - 1)
-        ]
-
--- arr !? (y + 1) >>= (\e -> e !? y + 1)
-
-safe2dLookup :: Array2d a -> (Int, Int) -> Maybe (a, (Int, Int))
-safe2dLookup arr (y, x) = do
-  row <- arr !? y
-  item <- row !? x
-  return (item, (y, x))
 
 part2 :: Input -> IO Int
-part2 _ = return 0
+part2 input = return $ sum $ findUnique <$> trails
+ where
+  arr = toArray2d input
+  trailHeads = findIndexes2d (== 0) input
+  trails = dfs (getNeighbours arr) <$> trailHeads
+
+findUnique :: [Node] -> Int
+findUnique nodes = go $ groupOn fst nodes
+ where
+  go :: [[Node]] -> Int
+  go [] = 0
+  go [cur] = length cur
+  go (cur : next : rest) = sum $ gogo <$> cur
+   where
+    gogo x =
+      let neighbours = filter (isNeighbour x) next
+       in go (neighbours : rest)
+
+isNeighbour :: Node -> Node -> Bool
+isNeighbour (_, (y, x)) (_, c) = c `elem` [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]
+
+getNeighbours :: Array2d Int -> Node -> [Node]
+getNeighbours arr (item, (y, x)) =
+  filter ((== item) . pred . fst) $
+    catMaybes
+      [ safe2dLookup arr (y + 1, x)
+      , safe2dLookup arr (y - 1, x)
+      , safe2dLookup arr (y, x + 1)
+      , safe2dLookup arr (y, x - 1)
+      ]
 
 type Input = [[Int]]
 parser :: Parser Input
@@ -50,3 +63,9 @@ parser = many lineP
 
 test :: IO (Int, Int)
 test = testSolution solution
+
+safe2dLookup :: Array2d a -> (Int, Int) -> Maybe (a, (Int, Int))
+safe2dLookup arr (y, x) = do
+  row <- arr !? y
+  item <- row !? x
+  return (item, (y, x))
