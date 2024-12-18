af2872cf868d7cd2d4e74cdbef0a8d80ab4bbffd
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-18 21:26:05 +0100

    day 17 part2 wip

--- src/Solutions/Day17.hs
+++ src/Solutions/Day17.hs
@@ -5,7 +5,7 @@ module Solutions.Day17 (solution, test) where
 import Text.Megaparsec.Char (char, digitChar, eol, newline, string)
 
 import Control.Monad.Writer (MonadWriter (..), Writer, execWriter)
-import Data.Array (Array, bounds, listArray, (!))
+import Data.Array (Array, bounds, elems, listArray, (!))
 import Data.Bits (xor)
 import Data.Char (digitToInt)
 import Data.List (intercalate)
@@ -15,16 +15,24 @@ import Lib.Solution
 import Text.Megaparsec (sepBy1)
 import Text.Megaparsec.Char.Lexer (decimal)
 
-solution :: Solution Input String String
+solution :: Solution Input String Int
 solution = Solution 17 parser part1 part2
 
 part1 :: Input -> IO String
-part1 (a, b) = do
-  let res = execWriter (runProgram b a)
-  return $ intercalate "," res
+part1 (initialState, instructions) = do
+  let res = execWriter (runProgram instructions initialState)
+  return $ intercalate "," $ show <$> res
 
-part2 :: Input -> IO String
-part2 = todo
+part2 :: Input -> IO Int
+part2 (initialState, instructions) = do
+  -- let res = execWriter (runProgram instructions initialState{regA = 117440})
+  -- print res
+  -- print $ res == elems instructions
+
+  return $ fst $ head $ filter snd $ zip [0 ..] results
+ where
+  check n = elems instructions == execWriter (runProgram instructions initialState{regA = n})
+  results = check <$> [0 ..]
 
 data ComputerState = ComputerState {regA :: Int, regB :: Int, regC :: Int, ip :: Int} deriving (Show)
 
@@ -38,7 +46,7 @@ getOpValue s 5 = s.regB
 getOpValue s 6 = s.regC
 getOpValue _ _ = error "invalid operand"
 
-runProgram :: Array Int Int -> ComputerState -> Writer [String] ComputerState
+runProgram :: Array Int Int -> ComputerState -> Writer [Int] ComputerState
 runProgram instructions s =
   if s.ip <= snd (bounds instructions)
     then do
@@ -49,7 +57,7 @@ runProgram instructions s =
   cmd = instructions ! s.ip
   op = instructions ! (s.ip + 1)
   combo = getOpValue s op
-  runCmd :: Int -> Writer [String] ComputerState
+  runCmd :: Int -> Writer [Int] ComputerState
   runCmd 0 = return s{regA = s.regA `div` (2 `pow` combo), ip = s.ip + 2}
   runCmd 1 = return s{regB = s.regB `xor` op, ip = s.ip + 2}
   runCmd 2 = return s{regB = combo `mod` 8, ip = s.ip + 2}
@@ -59,7 +67,8 @@ runProgram instructions s =
       else return s{ip = op}
   runCmd 4 = return $ s{regB = s.regB `xor` s.regC, ip = s.ip + 2}
   runCmd 5 = do
-    tell [show (combo `mod` 8)]
+    let res = combo `mod` 8
+    tell [res]
     return s{ip = s.ip + 2}
   runCmd 6 = return s{regB = s.regA `div` (2 `pow` combo), ip = s.ip + 2}
   runCmd 7 = return s{regC = s.regA `div` (2 `pow` combo), ip = s.ip + 2}
@@ -78,5 +87,5 @@ parser = do
   instructions <- digitChar `sepBy1` char ','
   return (ComputerState{regA = a, regB = b, regC = c, ip = 0}, listArray (0, length instructions - 1) (digitToInt <$> instructions))
 
-test :: IO (String, String)
+test :: IO (String, Int)
 test = testSolution solution
