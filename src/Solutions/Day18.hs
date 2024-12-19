692aca1c4ea73bac542448c670eb4cab015202dd
Hans Fredrik Furholt <hansff@gmail.com>
2024-12-19 01:06:50 +0100

    day 18 part 2

--- src/Solutions/Day18.hs
+++ src/Solutions/Day18.hs
@@ -8,25 +8,29 @@ import Control.Monad (guard)
 import Data.Array (assocs)
 import Data.Maybe (catMaybes)
 import Lib.Array2d (Array2d, replaceArr2D, safe2dLookup, toArray2d)
+import Lib.Dfs (dfs)
 import Lib.Dijkstra (shortestPath)
 import Lib.Parser (Parser)
 import Lib.Solution
 import Text.Megaparsec (many)
 import Text.Megaparsec.Char.Lexer (decimal)
 
-solution :: Solution Input Int Int
+solution :: Solution Input Int (Int, Int)
 solution = Solution 18 parser part1 part2
+
+boardSize :: Int
+boardSize = 71
+end :: (Int, Int)
+end = (boardSize - 1, boardSize - 1)
 
 part1 :: Input -> IO Int
 part1 input = do
-  return $ shortestPath (getNeighbours arr) vertexes (0, 0) (70, 70)
+  return $ shortestPath (getNeighbours arr) vertexes (0, 0) end
  where
-  boardSize = 71
   iterations = 1024
   byteLocations = take iterations input
   arr' = toArray2d $ replicate boardSize $ replicate boardSize Open
   arr = foldr (\(x, y) -> replaceArr2D y x Wall) arr' byteLocations
-  -- getNeighbours' x = (\(_, a) -> (1, (Open, a))) <$> getNeighbours (const . not . isWall) arr x
   vertexes = do
     (y, a) <- assocs arr
     (x, b) <- assocs a
@@ -35,6 +39,12 @@ part1 input = do
 
 getNeighbours :: Array2d Tile -> (Int, Int) -> [(Int, (Int, Int))]
 getNeighbours arr (x, y) = (1,) . snd <$> b
+ where
+  a = catMaybes [safe2dLookup arr (x + 1, y), safe2dLookup arr (x - 1, y), safe2dLookup arr (x, y + 1), safe2dLookup arr (x, y - 1)]
+  b = filter (\(t, _) -> not $ isWall t) a
+
+getNeighbours2 :: Array2d Tile -> (Int, Int) -> [(Int, Int)]
+getNeighbours2 arr (x, y) = snd <$> b
  where
   a = catMaybes [safe2dLookup arr (x + 1, y), safe2dLookup arr (x - 1, y), safe2dLookup arr (x, y + 1), safe2dLookup arr (x, y - 1)]
   b = filter (\(t, _) -> not $ isWall t) a
@@ -48,8 +58,18 @@ isWall :: Tile -> Bool
 isWall Wall = True
 isWall _ = False
 
-part2 :: Input -> IO Int
-part2 input = return 0
+part2 :: Input -> IO (Int, Int)
+part2 input = do
+  return $ go arr input
+ where
+  arr = toArray2d $ replicate boardSize $ replicate boardSize Open
+
+go :: Array2d Tile -> [(Int, Int)] -> (Int, Int)
+go _ [] = error "out of bytes"
+go prev (b@(x, y) : bs) = if end `elem` a then go arr bs else b
+ where
+  arr = replaceArr2D y x Wall prev
+  a = dfs (getNeighbours2 arr) (0, 0)
 
 type Input = [(Int, Int)]
 parser :: Parser Input
@@ -60,5 +80,5 @@ parser = many p
     y <- decimal <* eol
     return (x, y)
 
-test :: IO (Int, Int)
+test :: IO (Int, (Int, Int))
 test = testSolution solution
